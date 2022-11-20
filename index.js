57b5784f4ccc118c2bdb90d6910099c45a49b158
shubhams3423 <shubhams1671h@gmail.com>
2022-11-20 12:34:38 +0530

    counter app

---
+++ index.js
@@ -0,0 +1,31 @@
+ let zero=0;
+ function minus(){
+    zero=zero-1;
+    // console.log(zero)
+    document.getElementById('zero').innerHTML = zero;
+ }
+function reset(){
+    zero=0;
+    console.log('reset')
+   document.getElementById('zero').innerHTML =zero;
+
+}
+function plus()
+{
+    zero=zero+1;
+    document.getElementById('zero').innerHTML = zero;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
